49af38bb8cfc68aa7a76088c8812d815a0e4f66f
KvRae <karamelmannai@gmail.com>
2024-08-10 14:06:00 +0100

    Working on features:
    Snap fingers to delete half of project files
    Snp hands to wipe all files
    
    Readme file content update

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.kvrae"
-version = "1.0-SNAPSHOT"
+version = "1.0-DEV"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/Kanos.kt
+++ src/main/kotlin/Kanos.kt
@@ -1,12 +1,45 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
 
 
 class Kanos: Plugin<Project> {
     override fun apply(target: Project) {
-        target.tasks.register("kanos") {
-            it.doLast {
-                println("Hello, Kanos!")
+        // Register the tasks
+        target.tasks.register("kanos --snap-fingers") {
+            it.doLast { task ->
+                handleFiles(task, target, deleteCondition = true)
+            }
+        }
+
+        target.tasks.register("kanos --snap-hands") {
+            it.doLast { task ->
+                handleFiles(task, target, deleteCondition = false)
+            }
+        }
+    }
+
+    /**
+     * Handles files in the project directory based on the provided conditions.
+     *
+     * @param task The task that is being executed.
+     * @param target The target project.
+     * @param deleteCondition Determines whether to use the delete condition.
+     */
+    private fun handleFiles(task: Task, target: Project, deleteCondition: Boolean) {
+        task.project.logger.quiet("Kanos is here")
+        target.fileTree(target.projectDir).visit { fileVisitDetails ->
+            val dirSize = fileVisitDetails.size
+            if (dirSize > 0) {
+                val shouldDelete = if (deleteCondition) {
+                    (dirSize % 2).toInt() == 0
+                } else {
+                    true
+                }
+                if (shouldDelete) {
+                    fileVisitDetails.file.delete()
+                    target.logger.quiet("Deleted file: ${fileVisitDetails.file}")
+                }
             }
         }
     }
